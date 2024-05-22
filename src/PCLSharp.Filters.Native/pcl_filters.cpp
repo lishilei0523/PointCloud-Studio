4e0a147080e92f2df6ef09171576db792ea5546f
Lee <lishilei0523@163.com>
2024-05-22 10:46:35 +0800

    补充点云滤波本地接口；

--- src/PCLSharp.Filters.Native/pcl_filters.cpp
+++ src/PCLSharp.Filters.Native/pcl_filters.cpp
@@ -1,9 +1,12 @@
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 #include <pcl/filters/passthrough.h>
+#include <pcl/filters/random_sample.h>
 #include <pcl/filters/uniform_sampling.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/approximate_voxel_grid.h>
 #include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/filters/radius_outlier_removal.h>
 #include <primitives_map.h>
 #include "pcl_filters.h"
 using namespace std;
@@ -33,6 +36,35 @@ Point3Fs* applyPassThrogh(Point3F points[], const int length, const char* axis, 
 	passThrough.setFilterFieldName(axis);				//设置过滤坐标轴
 	passThrough.setFilterLimits(limitMin, limixMax);	//设置过滤范围
 	passThrough.filter(*targetCloud);
+
+	Point3Fs* point3Fs = pclsharp::toPoint3Fs(*targetCloud);
+
+	return point3Fs;
+}
+
+/// <summary>
+/// 适用随机采样
+/// </summary>
+/// <param name="points">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="seed">随机种子</param>
+/// <param name="samplesCount">采样数量</param>
+/// <returns>过滤后点集</returns>
+Point3Fs* applyRandomSampling(Point3F points[], const int length, const int seed, const int samplesCount)
+{
+	const PointCloud<PointXYZ>::Ptr sourceCloud = std::make_shared<PointCloud<PointXYZ>>();
+	const PointCloud<PointXYZ>::Ptr targetCloud = std::make_shared<PointCloud<PointXYZ>>();
+
+	//加载点云
+	const PointCloud<PointXYZ>& pointCloud = pclsharp::toPointCloud(points, length);
+	copyPointCloud(pointCloud, *sourceCloud);
+
+	//随机采样
+	RandomSample<PointXYZ> randomSampling;
+	randomSampling.setInputCloud(sourceCloud);
+	randomSampling.setSeed(seed);
+	randomSampling.setSample(samplesCount);
+	randomSampling.filter(*targetCloud);
 
 	Point3Fs* point3Fs = pclsharp::toPoint3Fs(*targetCloud);
 
@@ -94,14 +126,13 @@ Point3Fs* applyVoxelGrid(Point3F points[], const int length, const float leafSiz
 }
 
 /// <summary>
-/// 适用离群点移除
+/// 适用近似体素降采样
 /// </summary>
 /// <param name="points">点集</param>
 /// <param name="length">点集长度</param>
-/// <param name="meanK">平均距离估计的最近邻居的数量</param>
-/// <param name="stddevMult">标准差阈值系数</param>
+/// <param name="leafSize">叶尺寸</param>
 /// <returns>过滤后点集</returns>
-Point3Fs* applyOutlierRemoval(Point3F points[], const int length, const int meanK, const float stddevMult)
+Point3Fs* applyApproximateVoxelGrid(Point3F points[], int length, float leafSize)
 {
 	const PointCloud<PointXYZ>::Ptr sourceCloud = std::make_shared<PointCloud<PointXYZ>>();
 	const PointCloud<PointXYZ>::Ptr targetCloud = std::make_shared<PointCloud<PointXYZ>>();
@@ -110,12 +141,69 @@ Point3Fs* applyOutlierRemoval(Point3F points[], const int length, const int mean
 	const PointCloud<PointXYZ>& pointCloud = pclsharp::toPointCloud(points, length);
 	copyPointCloud(pointCloud, *sourceCloud);
 
-	//初始化统计学离群点移除过滤器
+	//近似体素降采样
+	ApproximateVoxelGrid<PointXYZ> approximateVoxelGrid;
+	approximateVoxelGrid.setInputCloud(sourceCloud);
+	approximateVoxelGrid.setLeafSize(leafSize, leafSize, leafSize);
+	approximateVoxelGrid.filter(*targetCloud);
+
+	Point3Fs* point3Fs = pclsharp::toPoint3Fs(*targetCloud);
+
+	return point3Fs;
+}
+
+/// <summary>
+/// 适用统计离群点移除
+/// </summary>
+/// <param name="points">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="meanK">平均距离估计的最近邻居的数量</param>
+/// <param name="stddevMult">标准差阈值系数</param>
+/// <returns>过滤后点集</returns>
+Point3Fs* applyStatisticalOutlierRemoval(Point3F points[], const int length, const int meanK, const float stddevMult)
+{
+	const PointCloud<PointXYZ>::Ptr sourceCloud = std::make_shared<PointCloud<PointXYZ>>();
+	const PointCloud<PointXYZ>::Ptr targetCloud = std::make_shared<PointCloud<PointXYZ>>();
+
+	//加载点云
+	const PointCloud<PointXYZ>& pointCloud = pclsharp::toPointCloud(points, length);
+	copyPointCloud(pointCloud, *sourceCloud);
+
+	//统计离群点移除
 	StatisticalOutlierRemoval<PointXYZ> statisticalOutlierRemoval;
 	statisticalOutlierRemoval.setInputCloud(sourceCloud);
-	statisticalOutlierRemoval.setMeanK(50);//设置平均距离估计的最近邻居的数量K	
-	statisticalOutlierRemoval.setStddevMulThresh(1.0);//设置标准差阈值系数
-	statisticalOutlierRemoval.filter(*targetCloud);	//执行过滤
+	statisticalOutlierRemoval.setMeanK(meanK);
+	statisticalOutlierRemoval.setStddevMulThresh(stddevMult);
+	statisticalOutlierRemoval.filter(*targetCloud);
+
+	Point3Fs* point3Fs = pclsharp::toPoint3Fs(*targetCloud);
+
+	return point3Fs;
+}
+
+/// <summary>
+/// 适用半径离群点移除
+/// </summary>
+/// <param name="points">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="radius">搜索半径</param>
+/// <param name="minNeighborsInRadius">半径范围内点数量最小值</param>
+/// <returns>过滤后点集</returns>
+Point3Fs* applyRadiusOutlierRemoval(Point3F points[], int length, const float radius, const int minNeighborsInRadius)
+{
+	const PointCloud<PointXYZ>::Ptr sourceCloud = std::make_shared<PointCloud<PointXYZ>>();
+	const PointCloud<PointXYZ>::Ptr targetCloud = std::make_shared<PointCloud<PointXYZ>>();
+
+	//加载点云
+	const PointCloud<PointXYZ>& pointCloud = pclsharp::toPointCloud(points, length);
+	copyPointCloud(pointCloud, *sourceCloud);
+
+	//半径离群点移除
+	RadiusOutlierRemoval<PointXYZ> radiusOutlierRemoval;
+	radiusOutlierRemoval.setInputCloud(sourceCloud);
+	radiusOutlierRemoval.setRadiusSearch(radius);
+	radiusOutlierRemoval.setMinNeighborsInRadius(minNeighborsInRadius);
+	radiusOutlierRemoval.filter(*targetCloud);
 
 	Point3Fs* point3Fs = pclsharp::toPoint3Fs(*targetCloud);
 
--- src/PCLSharp.Filters.Native/pcl_filters.h
+++ src/PCLSharp.Filters.Native/pcl_filters.h
@@ -16,6 +16,16 @@
 EXPORT_C Point3Fs* CALLING_MODE applyPassThrogh(Point3F points[], int length, const char* axis, float limitMin, float limixMax);
 
 /// <summary>
+/// 适用随机采样
+/// </summary>
+/// <param name="points">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="seed">随机种子</param>
+/// <param name="samplesCount">采样数量</param>
+/// <returns>过滤后点集</returns>
+EXPORT_C Point3Fs* CALLING_MODE applyRandomSampling(Point3F points[], int length, int seed, int samplesCount);
+
+/// <summary>
 /// 适用均匀采样
 /// </summary>
 /// <param name="points">点集</param>
@@ -34,14 +44,33 @@ EXPORT_C Point3Fs* CALLING_MODE applyUniformSampling(Point3F points[], int lengt
 EXPORT_C Point3Fs* CALLING_MODE applyVoxelGrid(Point3F points[], int length, float leafSize);
 
 /// <summary>
-/// 适用离群点移除
+/// 适用近似体素降采样
+/// </summary>
+/// <param name="points">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="leafSize">叶尺寸</param>
+/// <returns>过滤后点集</returns>
+EXPORT_C Point3Fs* CALLING_MODE applyApproximateVoxelGrid(Point3F points[], int length, float leafSize);
+
+/// <summary>
+/// 适用统计离群点移除
 /// </summary>
 /// <param name="points">点集</param>
 /// <param name="length">点集长度</param>
 /// <param name="meanK">平均距离估计的最近邻居的数量</param>
 /// <param name="stddevMult">标准差阈值系数</param>
 /// <returns>过滤后点集</returns>
-EXPORT_C Point3Fs* CALLING_MODE applyOutlierRemoval(Point3F points[], int length, int meanK, float stddevMult);
+EXPORT_C Point3Fs* CALLING_MODE applyStatisticalOutlierRemoval(Point3F points[], int length, int meanK, float stddevMult);
+
+/// <summary>
+/// 适用半径离群点移除
+/// </summary>
+/// <param name="points">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="radius">搜索半径</param>
+/// <param name="minNeighborsInRadius">半径范围内点数量最小值</param>
+/// <returns>过滤后点集</returns>
+EXPORT_C Point3Fs* CALLING_MODE applyRadiusOutlierRemoval(Point3F points[], int length, float radius, int minNeighborsInRadius);
 
 /// <summary>
 /// 释放资源
