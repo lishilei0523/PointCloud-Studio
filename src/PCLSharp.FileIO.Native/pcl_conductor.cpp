0bb36756b11cf0479f193aed62aa789ff572f288
Lee <lishilei0523@163.com>
2024-05-23 12:49:20 +0800

    点云文件读写C++部分完善；

--- src/PCLSharp.FileIO.Native/pcl_conductor.cpp
+++ src/PCLSharp.FileIO.Native/pcl_conductor.cpp
@@ -29,6 +29,44 @@ Point3Fs* loadPCD(const char* filePath)
 }
 
 /// <summary>
+/// 加载法向量PCD文件
+/// </summary>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+Point3Normal3s* loadNormalPCD(const char* filePath)
+{
+	PointCloud<PointNormal> cloud;
+	const int& loadStatus = pcl::io::loadPCDFile(filePath, cloud);
+	if (loadStatus == 0)
+	{
+		Point3Normal3s* point3Normal3s = pclsharp::toPoint3Normal3s(cloud);
+		return point3Normal3s;
+	}
+
+	const string message = std::format("加载\"{}\"时出错！", filePath);
+	throw exception(message.c_str());
+}
+
+/// <summary>
+/// 加载颜色PCD文件
+/// </summary>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+Point3Color4s* loadColorPCD(const char* filePath)
+{
+	PointCloud<PointXYZRGBA> cloud;
+	const int& loadStatus = pcl::io::loadPCDFile(filePath, cloud);
+	if (loadStatus == 0)
+	{
+		Point3Color4s* point3Color4s = pclsharp::toPoint3Color4s(cloud);
+		return point3Color4s;
+	}
+
+	const string message = std::format("加载\"{}\"时出错！", filePath);
+	throw exception(message.c_str());
+}
+
+/// <summary>
 /// 加载PLY文件
 /// </summary>
 /// <param name="filePath">文件路径</param>
@@ -48,6 +86,44 @@ Point3Fs* loadPLY(const char* filePath)
 }
 
 /// <summary>
+/// 加载法向量PLY文件
+/// </summary>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+Point3Normal3s* loadNormalPLY(const char* filePath)
+{
+	PointCloud<PointNormal> cloud;
+	const int& loadStatus = pcl::io::loadPLYFile(filePath, cloud);
+	if (loadStatus == 0)
+	{
+		Point3Normal3s* point3Normal3s = pclsharp::toPoint3Normal3s(cloud);
+		return point3Normal3s;
+	}
+
+	const string message = std::format("加载\"{}\"时出错！", filePath);
+	throw exception(message.c_str());
+}
+
+/// <summary>
+/// 加载颜色PLY文件
+/// </summary>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+Point3Color4s* loadColorPLY(const char* filePath)
+{
+	PointCloud<PointXYZRGBA> cloud;
+	const int& loadStatus = pcl::io::loadPLYFile(filePath, cloud);
+	if (loadStatus == 0)
+	{
+		Point3Color4s* point3Color4s = pclsharp::toPoint3Color4s(cloud);
+		return point3Color4s;
+	}
+
+	const string message = std::format("加载\"{}\"时出错！", filePath);
+	throw exception(message.c_str());
+}
+
+/// <summary>
 /// 加载OBJ文件
 /// </summary>
 /// <param name="filePath">文件路径</param>
@@ -60,6 +136,44 @@ Point3Fs* loadOBJ(const char* filePath)
 	{
 		Point3Fs* point3Fs = pclsharp::toPoint3Fs(cloud);
 		return point3Fs;
+	}
+
+	const string message = std::format("加载\"{}\"时出错！", filePath);
+	throw exception(message.c_str());
+}
+
+/// <summary>
+/// 加载法向量OBJ文件
+/// </summary>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+Point3Normal3s* loadNormalOBJ(const char* filePath)
+{
+	PointCloud<PointNormal> cloud;
+	const int& loadStatus = pcl::io::loadOBJFile(filePath, cloud);
+	if (loadStatus == 0)
+	{
+		Point3Normal3s* point3Normal3s = pclsharp::toPoint3Normal3s(cloud);
+		return point3Normal3s;
+	}
+
+	const string message = std::format("加载\"{}\"时出错！", filePath);
+	throw exception(message.c_str());
+}
+
+/// <summary>
+/// 加载颜色OBJ文件
+/// </summary>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+Point3Color4s* loadColorOBJ(const char* filePath)
+{
+	PointCloud<PointXYZRGBA> cloud;
+	const int& loadStatus = pcl::io::loadOBJFile(filePath, cloud);
+	if (loadStatus == 0)
+	{
+		Point3Color4s* point3Color4s = pclsharp::toPoint3Color4s(cloud);
+		return point3Color4s;
 	}
 
 	const string message = std::format("加载\"{}\"时出错！", filePath);
@@ -103,6 +217,78 @@ void saveBinaryPCD(Point3F points[], int length, const char* filePath)
 }
 
 /// <summary>
+/// 保存法向量PCD文本文件
+/// </summary>
+/// <param name="pointNormals">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+void saveNormalTextPCD(Point3Normal3 pointNormals[], int length, const char* filePath)
+{
+	const PointCloud<PointNormal>& cloud = pclsharp::toPointCloud(pointNormals, length);
+	const int& saveStatus = pcl::io::savePCDFileASCII(filePath, cloud);
+	if (saveStatus != 0)
+	{
+		const string message = std::format("保存\"{}\"时出错！", filePath);
+		throw exception(message.c_str());
+	}
+}
+
+/// <summary>
+/// 保存法向量PCD二进制文件
+/// </summary>
+/// <param name="pointNormals">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+void saveNormalBinaryPCD(Point3Normal3 pointNormals[], int length, const char* filePath)
+{
+	const PointCloud<PointNormal>& cloud = pclsharp::toPointCloud(pointNormals, length);
+	const int& saveStatus = pcl::io::savePCDFileBinaryCompressed(filePath, cloud);
+	if (saveStatus != 0)
+	{
+		const string message = std::format("保存\"{}\"时出错！", filePath);
+		throw exception(message.c_str());
+	}
+}
+
+/// <summary>
+/// 保存颜色PCD文本文件
+/// </summary>
+/// <param name="pointColors">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+void saveColorTextPCD(Point3Color4 pointColors[], int length, const char* filePath)
+{
+	const PointCloud<PointXYZRGBA>& cloud = pclsharp::toPointCloud(pointColors, length);
+	const int& saveStatus = pcl::io::savePCDFileASCII(filePath, cloud);
+	if (saveStatus != 0)
+	{
+		const string message = std::format("保存\"{}\"时出错！", filePath);
+		throw exception(message.c_str());
+	}
+}
+
+/// <summary>
+/// 保存颜色PCD二进制文件
+/// </summary>
+/// <param name="pointColors">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+void saveColorBinaryPCD(Point3Color4 pointColors[], int length, const char* filePath)
+{
+	const PointCloud<PointXYZRGBA>& cloud = pclsharp::toPointCloud(pointColors, length);
+	const int& saveStatus = pcl::io::savePCDFileBinaryCompressed(filePath, cloud);
+	if (saveStatus != 0)
+	{
+		const string message = std::format("保存\"{}\"时出错！", filePath);
+		throw exception(message.c_str());
+	}
+}
+
+/// <summary>
 /// 保存PLY文本文件
 /// </summary>
 /// <param name="points">点集</param>
@@ -130,6 +316,78 @@ void saveTextPLY(Point3F points[], int length, const char* filePath)
 void saveBinaryPLY(Point3F points[], int length, const char* filePath)
 {
 	const PointCloud<PointXYZ>& cloud = pclsharp::toPointCloud(points, length);
+	const int& saveStatus = pcl::io::savePLYFileBinary(filePath, cloud);
+	if (saveStatus != 0)
+	{
+		const string message = std::format("保存\"{}\"时出错！", filePath);
+		throw exception(message.c_str());
+	}
+}
+
+/// <summary>
+/// 保存法向量PLY文本文件
+/// </summary>
+/// <param name="pointNormals">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+void saveNormalTextPLY(Point3Normal3 pointNormals[], int length, const char* filePath)
+{
+	const PointCloud<PointNormal>& cloud = pclsharp::toPointCloud(pointNormals, length);
+	const int& saveStatus = pcl::io::savePLYFileASCII(filePath, cloud);
+	if (saveStatus != 0)
+	{
+		const string message = std::format("保存\"{}\"时出错！", filePath);
+		throw exception(message.c_str());
+	}
+}
+
+/// <summary>
+/// 保存法向量PLY二进制文件
+/// </summary>
+/// <param name="pointNormals">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+void saveNormalBinaryPLY(Point3Normal3 pointNormals[], int length, const char* filePath)
+{
+	const PointCloud<PointNormal>& cloud = pclsharp::toPointCloud(pointNormals, length);
+	const int& saveStatus = pcl::io::savePLYFileBinary(filePath, cloud);
+	if (saveStatus != 0)
+	{
+		const string message = std::format("保存\"{}\"时出错！", filePath);
+		throw exception(message.c_str());
+	}
+}
+
+/// <summary>
+/// 保存颜色PLY文本文件
+/// </summary>
+/// <param name="pointColors">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+void saveColorTextPLY(Point3Color4 pointColors[], int length, const char* filePath)
+{
+	const PointCloud<PointXYZRGBA>& cloud = pclsharp::toPointCloud(pointColors, length);
+	const int& saveStatus = pcl::io::savePLYFileASCII(filePath, cloud);
+	if (saveStatus != 0)
+	{
+		const string message = std::format("保存\"{}\"时出错！", filePath);
+		throw exception(message.c_str());
+	}
+}
+
+/// <summary>
+/// 保存颜色PLY二进制文件
+/// </summary>
+/// <param name="pointColors">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+void saveColorBinaryPLY(Point3Color4 pointColors[], int length, const char* filePath)
+{
+	const PointCloud<PointXYZRGBA>& cloud = pclsharp::toPointCloud(pointColors, length);
 	const int& saveStatus = pcl::io::savePLYFileBinary(filePath, cloud);
 	if (saveStatus != 0)
 	{
--- src/PCLSharp.FileIO.Native/pcl_conductor.h
+++ src/PCLSharp.FileIO.Native/pcl_conductor.h
@@ -3,6 +3,10 @@
 #define CALLING_MODE _cdecl
 #include <point3f.h>
 #include <point3fs.h>
+#include <point3normal3.h>
+#include <point3normal3s.h>
+#include <point3color4.h>
+#include <point3color4s.h>
 
 /// <summary>
 /// 加载PCD文件
@@ -12,6 +16,20 @@
 EXPORT_C Point3Fs* CALLING_MODE loadPCD(const char* filePath);
 
 /// <summary>
+/// 加载法向量PCD文件
+/// </summary>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+EXPORT_C Point3Normal3s* CALLING_MODE loadNormalPCD(const char* filePath);
+
+/// <summary>
+/// 加载颜色PCD文件
+/// </summary>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+EXPORT_C Point3Color4s* CALLING_MODE loadColorPCD(const char* filePath);
+
+/// <summary>
 /// 加载PLY文件
 /// </summary>
 /// <param name="filePath">文件路径</param>
@@ -19,11 +37,39 @@ EXPORT_C Point3Fs* CALLING_MODE loadPCD(const char* filePath);
 EXPORT_C Point3Fs* CALLING_MODE loadPLY(const char* filePath);
 
 /// <summary>
+/// 加载法向量PLY文件
+/// </summary>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+EXPORT_C Point3Normal3s* CALLING_MODE loadNormalPLY(const char* filePath);
+
+/// <summary>
+/// 加载颜色PLY文件
+/// </summary>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+EXPORT_C Point3Color4s* CALLING_MODE loadColorPLY(const char* filePath);
+
+/// <summary>
 /// 加载OBJ文件
 /// </summary>
 /// <param name="filePath">文件路径</param>
 /// <returns>点集</returns>
 EXPORT_C Point3Fs* CALLING_MODE loadOBJ(const char* filePath);
+
+/// <summary>
+/// 加载法向量OBJ文件
+/// </summary>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+EXPORT_C Point3Normal3s* CALLING_MODE loadNormalOBJ(const char* filePath);
+
+/// <summary>
+/// 加载颜色OBJ文件
+/// </summary>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+EXPORT_C Point3Color4s* CALLING_MODE loadColorOBJ(const char* filePath);
 
 /// <summary>
 /// 保存PCD文本文件
@@ -44,6 +90,42 @@ EXPORT_C void CALLING_MODE saveTextPCD(Point3F points[], int length, const char*
 EXPORT_C void CALLING_MODE saveBinaryPCD(Point3F points[], int length, const char* filePath);
 
 /// <summary>
+/// 保存法向量PCD文本文件
+/// </summary>
+/// <param name="pointNormals">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+EXPORT_C void CALLING_MODE saveNormalTextPCD(Point3Normal3 pointNormals[], int length, const char* filePath);
+
+/// <summary>
+/// 保存法向量PCD二进制文件
+/// </summary>
+/// <param name="pointNormals">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+EXPORT_C void CALLING_MODE saveNormalBinaryPCD(Point3Normal3 pointNormals[], int length, const char* filePath);
+
+/// <summary>
+/// 保存颜色PCD文本文件
+/// </summary>
+/// <param name="pointColors">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+EXPORT_C void CALLING_MODE saveColorTextPCD(Point3Color4 pointColors[], int length, const char* filePath);
+
+/// <summary>
+/// 保存颜色PCD二进制文件
+/// </summary>
+/// <param name="pointColors">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+EXPORT_C void CALLING_MODE saveColorBinaryPCD(Point3Color4 pointColors[], int length, const char* filePath);
+
+/// <summary>
 /// 保存PLY文本文件
 /// </summary>
 /// <param name="points">点集</param>
@@ -60,6 +142,42 @@ EXPORT_C void CALLING_MODE saveTextPLY(Point3F points[], int length, const char*
 /// <param name="filePath">文件路径</param>
 /// <returns>点集</returns>
 EXPORT_C void CALLING_MODE saveBinaryPLY(Point3F points[], int length, const char* filePath);
+
+/// <summary>
+/// 保存法向量PLY文本文件
+/// </summary>
+/// <param name="pointNormals">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+EXPORT_C void CALLING_MODE saveNormalTextPLY(Point3Normal3 pointNormals[], int length, const char* filePath);
+
+/// <summary>
+/// 保存法向量PLY二进制文件
+/// </summary>
+/// <param name="pointNormals">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+EXPORT_C void CALLING_MODE saveNormalBinaryPLY(Point3Normal3 pointNormals[], int length, const char* filePath);
+
+/// <summary>
+/// 保存颜色PLY文本文件
+/// </summary>
+/// <param name="pointColors">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+EXPORT_C void CALLING_MODE saveColorTextPLY(Point3Color4 pointColors[], int length, const char* filePath);
+
+/// <summary>
+/// 保存颜色PLY二进制文件
+/// </summary>
+/// <param name="pointColors">点集</param>
+/// <param name="length">点集长度</param>
+/// <param name="filePath">文件路径</param>
+/// <returns>点集</returns>
+EXPORT_C void CALLING_MODE saveColorBinaryPLY(Point3Color4 pointColors[], int length, const char* filePath);
 
 /// <summary>
 /// 释放资源
