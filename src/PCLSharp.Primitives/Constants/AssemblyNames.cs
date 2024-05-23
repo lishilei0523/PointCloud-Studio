e8f9dac2594ec354b2ef4a27ae3ae6f1bb3c96f2
Lee <lishilei0523@163.com>
2024-05-23 20:40:06 +0800

    增加特征程序集常量；

--- src/PCLSharp.Primitives/Constants/AssemblyNames.cs
+++ src/PCLSharp.Primitives/Constants/AssemblyNames.cs
@@ -34,5 +34,15 @@
 #if NETSTANDARD2_0_OR_GREATER
         public const string Normals = "PCLSharp.Normals.Native";
 #endif
+
+        /// <summary>
+        /// 特征程序集
+        /// </summary>
+#if NET462_OR_GREATER
+        public const string Features = "PCLSharp.Features.Native.dll";
+#endif
+#if NETSTANDARD2_0_OR_GREATER
+        public const string Features = "PCLSharp.Features.Native";
+#endif
     }
 }
