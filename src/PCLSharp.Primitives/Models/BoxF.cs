ed08e47587d5363a71888898c9813dd006f471f9
Lee <lishilei0523@163.com>
2024-05-29 10:32:47 +0800

    增加Box结构；

---
+++ src/PCLSharp.Primitives/Models/BoxF.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PCLSharp.Primitives.Models
+{
+    /// <summary>
+    /// 长方体
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public readonly struct BoxF
+    {
+        /// <summary>
+        /// 创建长方体构造器
+        /// </summary>
+        /// <param name="minPoint">最小坐标点</param>
+        /// <param name="maxPoint">最大坐标点</param>
+        public BoxF(Point3F minPoint, Point3F maxPoint)
+            : this()
+        {
+            this.MinPoint = minPoint;
+            this.MaxPoint = maxPoint;
+        }
+
+        /// <summary>
+        /// 最小坐标点
+        /// </summary>
+        public readonly Point3F MinPoint;
+
+        /// <summary>
+        /// 最大坐标点
+        /// </summary>
+        public readonly Point3F MaxPoint;
+    }
+}
