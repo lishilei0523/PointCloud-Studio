887042f3e7ef3f746626bcb7d0ec93b30ae89d8e
Lee <lishilei0523@163.com>
2024-05-29 21:04:10 +0800

    点云释放声明补充；

--- src/PCLSharp.Modules/Declarations/DisposeNative.cs
+++ src/PCLSharp.Modules/Declarations/DisposeNative.cs
@@ -27,6 +27,16 @@ namespace PCLSharp.Modules.Declarations
         public static extern void DisposePoint3Fs(IntPtr pointer);
         #endregion
 
+        #region # 释放坐标点集分组 —— static extern void DisposePoint3FsGroup(IntPtr pointer...
+        /// <summary>
+        /// 释放坐标点集分组
+        /// </summary>
+        /// <param name="pointer">二级指针</param>
+        /// <param name="groupCount">分组数</param>
+        [DllImport(AssemblyNames.Modules, EntryPoint = "disposePoint3FsGroup")]
+        public static extern void DisposePoint3FsGroup(IntPtr pointer, int groupCount);
+        #endregion
+
         #region # 释放法向量 —— static extern void DisposeNormal3F(IntPtr pointer)
         /// <summary>
         /// 释放法向量
@@ -43,6 +53,16 @@ namespace PCLSharp.Modules.Declarations
         /// <param name="pointer">指针</param>
         [DllImport(AssemblyNames.Modules, EntryPoint = "disposeNormal3Fs")]
         public static extern void DisposeNormal3Fs(IntPtr pointer);
+        #endregion
+
+        #region # 释放法向量集分组 —— static extern void DisposeNormal3FsGroup(IntPtr pointer...
+        /// <summary>
+        /// 释放法向量集分组
+        /// </summary>
+        /// <param name="pointer">二级指针</param>
+        /// <param name="groupCount">分组数</param>
+        [DllImport(AssemblyNames.Modules, EntryPoint = "disposeNormal3FsGroup")]
+        public static extern void DisposeNormal3FsGroup(IntPtr pointer, int groupCount);
         #endregion
 
         #region # 释放坐标点法向量 —— static extern void DisposePoint3Normal3(IntPtr pointer)
@@ -63,6 +83,16 @@ namespace PCLSharp.Modules.Declarations
         public static extern void DisposePoint3Normal3s(IntPtr pointer);
         #endregion
 
+        #region # 释放坐标点法向量集分组 —— static extern void DisposePoint3Normal3sGroup(IntPtr pointer...
+        /// <summary>
+        /// 释放坐标点法向量集分组
+        /// </summary>
+        /// <param name="pointer">二级指针</param>
+        /// <param name="groupCount">分组数</param>
+        [DllImport(AssemblyNames.Modules, EntryPoint = "disposePoint3Normal3sGroup")]
+        public static extern void DisposePoint3Normal3sGroup(IntPtr pointer, int groupCount);
+        #endregion
+
         #region # 释放坐标点颜色 —— static extern void DisposePoint3Color4(IntPtr pointer)
         /// <summary>
         /// 释放坐标点颜色
@@ -79,6 +109,16 @@ namespace PCLSharp.Modules.Declarations
         /// <param name="pointer">指针</param>
         [DllImport(AssemblyNames.Modules, EntryPoint = "disposePoint3Color4s")]
         public static extern void DisposePoint3Color4s(IntPtr pointer);
+        #endregion
+
+        #region # 释放坐标点颜色集分组 —— static extern void DisposePoint3Color4sGroup(IntPtr pointer...
+        /// <summary>
+        /// 释放坐标点颜色集分组
+        /// </summary>
+        /// <param name="pointer">二级指针</param>
+        /// <param name="groupCount">分组数</param>
+        [DllImport(AssemblyNames.Modules, EntryPoint = "disposePoint3Color4sGroup")]
+        public static extern void DisposePoint3Color4sGroup(IntPtr pointer, int groupCount);
         #endregion
 
         #region # 释放NARF描述子 —— static extern void DisposeNarf36F(IntPtr pointer)
