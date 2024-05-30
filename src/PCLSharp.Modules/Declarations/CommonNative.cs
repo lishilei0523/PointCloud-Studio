ed0656ba03a0ddc46722b52b8a7ac1872cea087d
Lee <lishilei0523@163.com>
2024-05-30 08:43:19 +0800

    长方体裁剪、凸包裁剪、投射平面、提取边框、提取边界C#部分接口实现；

--- src/PCLSharp.Modules/Declarations/CommonNative.cs
+++ src/PCLSharp.Modules/Declarations/CommonNative.cs
@@ -32,5 +32,75 @@ namespace PCLSharp.Modules.Declarations
         [DllImport(AssemblyNames.Modules, EntryPoint = "affineTransform")]
         public static extern IntPtr AffineTransform(Point3F[] points, int length, Pose pose);
         #endregion
+
+        #region # 长方体剪裁 —— static extern IntPtr CropBox(Point3F[] points...
+        /// <summary>
+        /// 长方体剪裁
+        /// </summary>
+        /// <param name="points">点集</param>
+        /// <param name="length">点集长度</param>
+        /// <param name="minPoint">最小坐标点</param>
+        /// <param name="maxPoint">最大坐标点</param>
+        /// <param name="negative">true: 剪裁/false: 保留</param>
+        /// <returns>剪裁后点云</returns>
+        [DllImport(AssemblyNames.Modules, EntryPoint = "cropBox")]
+        public static extern IntPtr CropBox(Point3F[] points, int length, Point3F minPoint, Point3F maxPoint, bool negative);
+        #endregion
+
+        #region # 凸包剪裁 —— static extern IntPtr CropConvexHull(Point3F[] points...
+        /// <summary>
+        /// 凸包剪裁
+        /// </summary>
+        /// <param name="points">点集</param>
+        /// <param name="length">点集长度</param>
+        /// <param name="contourPoints">轮廓点集</param>
+        /// <param name="contourLength">轮廓点集长度</param>
+        /// <param name="dimensionsCount">维度数</param>
+        /// <returns>剪裁后点云</returns>
+        [DllImport(AssemblyNames.Modules, EntryPoint = "cropConvexHull")]
+        public static extern IntPtr CropConvexHull(Point3F[] points, int length, Point3F[] contourPoints, int contourLength, int dimensionsCount);
+        #endregion
+
+        #region # 投射平面 —— static extern IntPtr ProjectPlane(Point3F[] points...
+        /// <summary>
+        /// 投射平面
+        /// </summary>
+        /// <param name="points">点集</param>
+        /// <param name="length">点集长度</param>
+        /// <param name="a">平面方程系数a</param>
+        /// <param name="b">平面方程系数b</param>
+        /// <param name="c">平面方程系数c</param>
+        /// <param name="d">平面方程系数d</param>
+        /// <returns>投射后点云</returns>
+        /// <remarks>平面方程: ax + by +cz + d = 0</remarks>
+        [DllImport(AssemblyNames.Modules, EntryPoint = "projectPlane")]
+        public static extern IntPtr ProjectPlane(Point3F[] points, int length, float a, float b, float c, float d);
+        #endregion
+
+        #region # 提取边框 —— static extern IntPtr ExtractBorder(Point3F[] points...
+        /// <summary>
+        /// 提取边框
+        /// </summary>
+        /// <param name="points">点集</param>
+        /// <param name="length">点集长度</param>
+        /// <returns>边框点云</returns>
+        [DllImport(AssemblyNames.Modules, EntryPoint = "extractBorder")]
+        public static extern IntPtr ExtractBorder(Point3F[] points, int length);
+        #endregion
+
+        #region # 提取边界 —— static extern IntPtr ExtractBoundary(Point3F[] points...
+        /// <summary>
+        /// 提取边界
+        /// </summary>
+        /// <param name="points">点集</param>
+        /// <param name="length">点集长度</param>
+        /// <param name="normalK">法向量K</param>
+        /// <param name="featureRadius">特征半径</param>
+        /// <param name="angleThreshold">角度阈值</param>
+        /// <param name="threadsCount">线程数</param>
+        /// <returns>边界点云</returns>
+        [DllImport(AssemblyNames.Modules, EntryPoint = "extractBoundary")]
+        public static extern IntPtr ExtractBoundary(Point3F[] points, int length, int normalK, float featureRadius, float angleThreshold, int threadsCount);
+        #endregion
     }
 }
--- src/PCLSharp.Modules/Implements/CloudCommon.cs
+++ src/PCLSharp.Modules/Implements/CloudCommon.cs
@@ -69,5 +69,164 @@ namespace PCLSharp.Modules.Implements
             return transformedPoints;
         }
         #endregion
+
+        #region # 长方体剪裁 —— Point3F[] CropBox(IEnumerable<Point3F> points...
+        /// <summary>
+        /// 长方体剪裁
+        /// </summary>
+        /// <param name="points">点集</param>
+        /// <param name="minPoint">最小坐标点</param>
+        /// <param name="maxPoint">最大坐标点</param>
+        /// <param name="negative">true: 剪裁/false: 保留</param>
+        /// <returns>剪裁后点云</returns>
+        public Point3F[] CropBox(IEnumerable<Point3F> points, Point3F minPoint, Point3F maxPoint, bool negative)
+        {
+            Point3F[] points_ = points?.ToArray() ?? Array.Empty<Point3F>();
+
+            #region # 验证
+
+            if (!points_.Any())
+            {
+                return Array.Empty<Point3F>();
+            }
+
+            #endregion
+
+            IntPtr pointer = CommonNative.CropBox(points_, points_.Length, minPoint, maxPoint, negative);
+            Point3Fs point3Fs = Marshal.PtrToStructure<Point3Fs>(pointer);
+            Point3F[] croppedPoints = point3Fs.Recover();
+            DisposeNative.DisposePoint3Fs(pointer);
+
+            return croppedPoints;
+        }
+        #endregion
+
+        #region # 凸包剪裁 —— Point3F[] CropConvexHull(IEnumerable<Point3F> points...
+        /// <summary>
+        /// 凸包剪裁
+        /// </summary>
+        /// <param name="points">点集</param>
+        /// <param name="contourPoints">轮廓点集</param>
+        /// <param name="dimensionsCount">维度数</param>
+        /// <returns>剪裁后点云</returns>
+        public Point3F[] CropConvexHull(IEnumerable<Point3F> points, IEnumerable<Point3F> contourPoints, int dimensionsCount)
+        {
+            Point3F[] points_ = points?.ToArray() ?? Array.Empty<Point3F>();
+            Point3F[] contourPoints_ = contourPoints?.ToArray() ?? Array.Empty<Point3F>();
+
+            #region # 验证
+
+            if (!points_.Any())
+            {
+                return Array.Empty<Point3F>();
+            }
+            if (!contourPoints_.Any())
+            {
+                return points_;
+            }
+
+            #endregion
+
+            IntPtr pointer = CommonNative.CropConvexHull(points_, points_.Length, contourPoints_, contourPoints_.Length, dimensionsCount);
+            Point3Fs point3Fs = Marshal.PtrToStructure<Point3Fs>(pointer);
+            Point3F[] croppedPoints = point3Fs.Recover();
+            DisposeNative.DisposePoint3Fs(pointer);
+
+            return croppedPoints;
+        }
+        #endregion
+
+        #region # 投射平面 —— Point3F[] ProjectPlane(IEnumerable<Point3F> points...
+        /// <summary>
+        /// 投射平面
+        /// </summary>
+        /// <param name="points">点集</param>
+        /// <param name="a">平面方程系数a</param>
+        /// <param name="b">平面方程系数b</param>
+        /// <param name="c">平面方程系数c</param>
+        /// <param name="d">平面方程系数d</param>
+        /// <returns>投射后点云</returns>
+        /// <remarks>平面方程: ax + by +cz + d = 0</remarks>
+        public Point3F[] ProjectPlane(IEnumerable<Point3F> points, float a, float b, float c, float d)
+        {
+            Point3F[] points_ = points?.ToArray() ?? Array.Empty<Point3F>();
+
+            #region # 验证
+
+            if (!points_.Any())
+            {
+                return Array.Empty<Point3F>();
+            }
+
+            #endregion
+
+            IntPtr pointer = CommonNative.ProjectPlane(points_, points_.Length, a, b, c, d);
+            Point3Fs point3Fs = Marshal.PtrToStructure<Point3Fs>(pointer);
+            Point3F[] projectedPoints = point3Fs.Recover();
+            DisposeNative.DisposePoint3Fs(pointer);
+
+            return projectedPoints;
+        }
+        #endregion
+
+        #region # 提取边框 —— Point3F[] ExtractBorder(IEnumerable<Point3F> points)
+        /// <summary>
+        /// 提取边框
+        /// </summary>
+        /// <param name="points">点集</param>
+        /// <returns>边框点云</returns>
+        public Point3F[] ExtractBorder(IEnumerable<Point3F> points)
+        {
+            Point3F[] points_ = points?.ToArray() ?? Array.Empty<Point3F>();
+
+            #region # 验证
+
+            if (!points_.Any())
+            {
+                return Array.Empty<Point3F>();
+            }
+
+            #endregion
+
+            IntPtr pointer = CommonNative.ExtractBorder(points_, points_.Length);
+            Point3Fs point3Fs = Marshal.PtrToStructure<Point3Fs>(pointer);
+            Point3F[] borderPoints = point3Fs.Recover();
+            DisposeNative.DisposePoint3Fs(pointer);
+
+            return borderPoints;
+        }
+        #endregion
+
+        #region # 提取边界 —— Point3F[] ExtractBoundary(IEnumerable<Point3F> points...
+        /// <summary>
+        /// 提取边界
+        /// </summary>
+        /// <param name="points">点集</param>
+        /// <param name="normalK">法向量K</param>
+        /// <param name="featureRadius">特征半径</param>
+        /// <param name="angleThreshold">角度阈值</param>
+        /// <param name="threadsCount">线程数</param>
+        /// <returns>边界点云</returns>
+        public Point3F[] ExtractBoundary(IEnumerable<Point3F> points, int normalK, float featureRadius, float angleThreshold, int threadsCount)
+        {
+            Point3F[] points_ = points?.ToArray() ?? Array.Empty<Point3F>();
+
+            #region # 验证
+
+            if (!points_.Any())
+            {
+                return Array.Empty<Point3F>();
+            }
+
+            #endregion
+
+            IntPtr pointer = CommonNative.ExtractBoundary(points_, points_.Length, normalK, featureRadius, angleThreshold, threadsCount);
+            Point3Fs point3Fs = Marshal.PtrToStructure<Point3Fs>(pointer);
+            Point3F[] boundaryPoints = point3Fs.Recover();
+            DisposeNative.DisposePoint3Fs(pointer);
+
+            return boundaryPoints;
+        }
+        #endregion
     }
 }
