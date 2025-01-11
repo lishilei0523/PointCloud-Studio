f024a400e07f6ee924434f9dd7d261f0675c4d6e
Lee <lishilei0523@163.com>
2025-01-11 18:07:53 +0800

    添加单元测试项目；

---
+++ src/PCLSharp.Tests/TestCases/CommonTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PCLSharp.Modules.Implements;
+using PCLSharp.Modules.Interfaces;
+using PCLSharp.Primitives.Models;
+
+namespace PCLSharp.Tests.TestCases
+{
+    /// <summary>
+    /// 点云通用操作测试
+    /// </summary>
+    [TestClass]
+    public class CommonTests
+    {
+        #region # 测试估算质心 —— void TestEstimateCentroid()
+        /// <summary>
+        /// 测试估算质心
+        /// </summary>
+        [TestMethod]
+        public void TestEstimateCentroid()
+        {
+            string filePath = "../../../../../assets/table_scene_lms400.pcd";
+
+            ICloudFiles cloudFiles = new CloudFiles();
+            Point3F[] cloud = cloudFiles.LoadPCD(filePath);
+
+            ICloudCommon cloudCommon = new CloudCommon();
+            Point3F centroid = cloudCommon.EstimateCentroid(cloud);
+
+            Assert.AreEqual(centroid, new Point3F(-0.0994279459f, -0.307722867f, -1.3541435f));
+        }
+        #endregion
+    }
+}
---
+++ src/PCLSharp.Tests/TestCases/FilesTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PCLSharp.Modules.Implements;
+using PCLSharp.Modules.Interfaces;
+using PCLSharp.Primitives.Models;
+
+namespace PCLSharp.Tests.TestCases
+{
+    /// <summary>
+    /// 点云读写测试
+    /// </summary>
+    [TestClass]
+    public class FilesTests
+    {
+        #region # 测试加载PCD文件 —— void TestLoadPCD()
+        /// <summary>
+        /// 测试加载PCD文件
+        /// </summary>
+        [TestMethod]
+        public void TestLoadPCD()
+        {
+            string filePath = "../../../../../assets/table_scene_lms400.pcd";
+            ICloudFiles cloudFiles = new CloudFiles();
+            Point3F[] cloud = cloudFiles.LoadPCD(filePath);
+
+            Assert.AreEqual(cloud.Length, 460400);
+        }
+        #endregion
+    }
+}
